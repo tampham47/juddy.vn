0c4998f2677c0c4a0c3d2733c261ec4a279ca300
Tam Pham <tampham47@live.com>
2013-11-22 23:03:39 +0700

    Chỉnh sửa lại project admin

---
+++ Juddy.Admin/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PbData.Business;
+using PbData.Entities;
+
+namespace Juddy.Admin.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            bn_Product bnProduct = new bn_Product();
+            var model = bnProduct.GetByTags("pbox");
+            return View(model);
+        }
+
+    }
+}
--- Juddy.Admin/Properties/AssemblyInfo.cs
+++ Juddy.Admin/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9cc73f51-d88c-4552-9d04-64036a474aac")]
+[assembly: Guid("a6dfd3e6-5f68-4deb-a667-5e4759e7db00")]
 
 // Version information for an assembly consists of the following four values:
 //
