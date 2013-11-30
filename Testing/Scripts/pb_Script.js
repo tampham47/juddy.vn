84680de6b76d8bf6be09511c9968515600b8abe7
Tam Pham <tampham47@live.com>
2013-11-30 23:48:17 +0700

    Delete 2 dự án testing và edit.

--- Testing/Scripts/pb_Script.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function (e) {
-    $('.main-menu-inner').hide(); //An menu sau khi load trang
-    $("#logo").find('#pbox-logo').click(function (e) {
-        $('.main-menu-inner').slideToggle(500);
-    });
-});
