434e6b3252005b1d2ae8572523b90bad44b48d40
Huynh Van Than <thanhv92@gmal.com>
2013-12-04 18:04:02 +0700

    Thêm chức năng xem tất cả user trong juddy.admin, phân trang

---
+++ Juddy.Admin/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using PbData.Business;
+using PbData.Entities;
+
+namespace Juddy.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+   
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllUser(int page = 1)
+        {
+    
+            bn_UserProfile bnUser = new bn_UserProfile();
+            var model = bnUser.GetAll();
+            int numberOfPage = 5;
+            ViewBag.CountPage = model.Count() / numberOfPage + 1;
+            ViewBag.page = page;
+            if(page==model.Count() / numberOfPage+1)
+                model = model.GetRange((page-1)*numberOfPage-(numberOfPage-(model.Count() % numberOfPage)), numberOfPage);
+            else
+            model = model.GetRange((page-1)*numberOfPage,numberOfPage);
+            return View(model);
+        }
+
+    }
+}
